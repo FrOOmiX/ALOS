aa2e2b0b798545cb276e6eeadf7233a234749072
sdechaum <steven.dechaume@etudiant.univ-lr.fr>
2018-01-16 10:23:53 +0100

    Update list command

--- dt.js
+++ dt.js
@@ -1,14 +1,20 @@
 module.exports = function dt( options ){
 
     this.add('role:dt, cmd:list', function list(msg, respond) {
-        respond(null, {answer: msg.cmd})
-    })
+        this.make$('dt').list$({},function (err, list) {
+            var data = [];
+            list.forEach(function (dt) {
+                data.push(dt)
+            });
+            respond(null, {succes: true, msg: "", data:data})
+        })
+    });
 
     this.add('role:dt, cmd:load', function load(msg, respond) {
         this.make('dt').load$(msg.id, function (err, dt) {
             respond(null, {succes: true, msg: "", data:[dt]})
         })
-    })
+    });
 
     this.add('role:dt, cmd:create', function create(msg, respond) {
         this.make('dt').data$(msg.data).save$(function (err, dt) {
@@ -16,4 +22,4 @@ module.exports = function dt( options ){
             })
 
     })
-}
+};
