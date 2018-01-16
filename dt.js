7338dcb6a26845285fdc9a8431a2671aae71297d
sdechaum <steven.dechaume@etudiant.univ-lr.fr>
2018-01-16 11:23:42 +0100

    Add update command

--- dt.js
+++ dt.js
@@ -1,7 +1,7 @@
 module.exports = function dt( options ){
 
     this.add('role:dt, cmd:list', function list(msg, respond) {
-        this.make$('dt').list$({},function (err, list) {
+        this.make('dt').list$({},function (err, list) {
             var data = [];
             list.forEach(function (dt) {
                 data.push(dt)
@@ -12,14 +12,46 @@ module.exports = function dt( options ){
 
     this.add('role:dt, cmd:load', function load(msg, respond) {
         this.make('dt').load$(msg.id, function (err, dt) {
-            respond(null, {succes: true, msg: "", data:[dt]})
+            if(dt != null)
+                respond(null, {succes: true, msg: "", data:[dt]})
+            else
+                respond(null, {succes: false, msg: "Cette demande de travaux n'existe pas", data:[]})
         })
     });
 
     this.add('role:dt, cmd:create', function create(msg, respond) {
         this.make('dt').data$(msg.data).save$(function (err, dt) {
                 respond(null, {succes: true, msg: "", data:[{id: dt.id}]})
-            })
+        })
+
+    });
+
+    this.add('role:dt, cmd:remove', function remove(msg, respond) {
+        this.make('dt').load$(msg.data.id, function (err, dt) {
+            if(dt.state == "En cours"){
+                this.make('dt').remove$(dt.id, function (err, dt) {
+                    respond(null, {succes: true, msg: "", data:[dt]})
+                })
+            }
+            else{
+                respond(null, {succes: false, msg: "Suppresion impossible la demande de travaux est terminée.", data:[dt]})
+            }
+        })
+    });
+
+    this.add('role:dt, cmd:update', function update(msg, respond) {
+        this.make('dt').load(msg.id, function (err, dt){
+            if(dt.state == "En cours"){
+                this.make('dt').data$(msg.data).save$(function (err, dt) {
+                    respond(null, {succes: true, msg: "", data:[dt]})
+                })
+            }
+            else{
+                respond(null, {succes: false, msg: "Suppresion impossible la demande de travaux est terminée.", data:[dt]})
+            }
+        })
 
     })
+
+
 };
